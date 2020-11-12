59203681eb647c4be0a632db7921e516ac88bce0
Luca Bucher <luca.bucher@bison-group.com>
2020-11-12 18:01:53 +0100

    heroku works

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ VOLUME ["/tmp", "/data", "/config", "/logs"]
 EXPOSE 8080
 ENV PORT=8080
 COPY backend/build/libs/jass-*.jar app.jar
-ENTRYPOINT ["sh", "-c", "java ${JAVA_OPTS} -jar /app.jar -Dserver.port=${PORT} ${0} ${@}"]
+CMD java -jar /app.jar
--- backend/src/main/java/ch/bbzw/jass/JassApplication.java
+++ backend/src/main/java/ch/bbzw/jass/JassApplication.java
@@ -1,7 +1,13 @@
 package ch.bbzw.jass;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 public class JassApplication {
@@ -10,4 +16,9 @@ public class JassApplication {
 		SpringApplication.run(JassApplication.class, args);
 	}
 
+	 @Bean
+	  public ErrorViewResolver customErrorViewResolver() {
+	    final ModelAndView redirectToIndexHtml = new ModelAndView("forward:/index.html", Collections.emptyMap(), HttpStatus.OK);
+	    return (request, status, model) -> status == HttpStatus.NOT_FOUND ? redirectToIndexHtml : null;
+	  }
 }
