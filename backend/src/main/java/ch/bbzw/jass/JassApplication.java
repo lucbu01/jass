142262e191bd90d3317a863d821befa69927d080
Luca Bucher <lucbu01@bluewin.ch>
2020-11-12 13:57:34 +0100

    basic project structure

---
+++ backend/src/main/java/ch/bbzw/jass/JassApplication.java
@@ -0,0 +1,13 @@
+package ch.bbzw.jass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JassApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ch/bbzw/jass/JassApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbzw.jass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
