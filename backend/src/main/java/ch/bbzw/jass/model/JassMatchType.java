06a4857500c3f3cf77f4114a641b1cffffed22a7
Luca Bucher <lucbu01@bluewin.ch>
2020-11-26 15:22:57 +0100

    update match type

--- backend/src/main/java/ch/bbzw/jass/model/JassMatchType.java
+++ backend/src/main/java/ch/bbzw/jass/model/JassMatchType.java
@@ -1,12 +1,33 @@
 package ch.bbzw.jass.model;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
 public enum JassMatchType {
-	CLAMP(2), SHIELD(2), ROSE(4), ACORN(1), UPSIDE_DOWN(3), DOWNSIDE_UP(3), GUSTI(5), MERRY(5), SLALOM(5);
+	CLAMP(2, JassColor.CLAMP), //
+	SHIELD(2, JassColor.SHIELD), //
+	ROSE(4, JassColor.ROSE), //
+	ACORN(1, JassColor.ACORN), //
+	UPSIDE_DOWN(3, JassDirection.UPSIDE_DOWN), //
+	DOWNSIDE_UP(3, JassDirection.DOWNSIDE_UP), //
+	GUSTI(5, JassDirection.GUSTI), //
+	MERRY(5, JassDirection.MERRY), //
+	SLALOM_UP(5, JassDirection.SLALOM_UP), //
+	SLALOM_DOWN(5, JassDirection.SLALOM_DOWN);
 
 	private final int multiplicator;
+	private final JassDirection direction;
+	private final JassColor trumpColor;
+
+	private JassMatchType(int multiplicator, JassDirection direction) {
+		this.trumpColor = null;
+		this.multiplicator = multiplicator;
+		this.direction = direction;
+	}
+
+	private JassMatchType(int multiplicator, JassColor trumpColor) {
+		this.direction = null;
+		this.multiplicator = multiplicator;
+		this.trumpColor = trumpColor;
+	}
 }
