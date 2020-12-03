b38299177810d77b3788ab44f9bd67ad9ca21707
Lars Ghezzi <lars_ghezzi@sluz.ch>
2020-12-03 13:05:21 +0100

    initial structure for playground

--- frontend/src/app/playground/playground.component.ts
+++ frontend/src/app/playground/playground.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./playground.component.scss']
 })
 export class PlaygroundComponent implements OnInit {
+  playerName1 = 'player1';
+  playerName2 = 'player2';
+  playerName3 = 'player3';
+  playerName4 = 'player4';
+  countTeam1 = 'count1';
+  countTeam2 = 'count2';
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
